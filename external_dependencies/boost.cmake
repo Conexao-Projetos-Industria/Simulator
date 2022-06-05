6e2aff6940dfa87e48d2d7e9f4153372e8033dbf
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2022-06-05 10:48:41 -0300

    Using boost with CMake's FetchContent. Fixed target linking to boost_beast.

--- external_dependencies/boost.cmake
+++ external_dependencies/boost.cmake
@@ -1,17 +1,10 @@
+include(FetchContent)
 
-#TODO try to find system boost first
-#TODO boost is built every cmake run
-ExternalProject_Add(boost_external
-    GIT_REPOSITORY https://github.com/boostorg/boost
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/external/boost/
-    UPDATE_COMMAND ""
-    # CONFIGURE_COMMAND ""
-    UPDATE_DISCONNECTED 1
+FetchContent_Declare(
+    boost
+    GIT_REPOSITORY https://github.com/boostorg/boost.git
+    GIT_TAG boost-1.79.0
+    GIT_PROGRESS TRUE
 )
 
-add_library(boost SHARED IMPORTED)
-add_dependencies(boost PUBLIC boost_external)
-target_include_directories(boost INTERFACE ${PROJECT_SOURCE_DIR}/external/boost/include/boost-1_79) #TODO boost version hardcoded
-set_target_properties(boost PROPERTIES IMPORTED_IMPLIB "${PROJECT_SOURCE_DIR}/external/boost/lib/libboost_date_time-vc143-mt-gd-x64-1_79.lib") #TODO boost lib name hardcoded
-
-set(BOOST_LIB boost)
+FetchContent_MakeAvailable(boost)
--- external_dependencies/external_dependencies.cmake
+++ external_dependencies/external_dependencies.cmake
@@ -5,22 +5,13 @@ find_package(OpenGL REQUIRED)
 include(ExternalProject)
 
 if(USING_CONAN)
-
     include(external_dependencies/run_conan.cmake)
-
-    find_package(Boost)
-    if (WIN32)
-        set(BOOST_LIB Boost::boost)
-    elseif (UNIX)
-        set(BOOST_LIB Boost::boost rt)
-    endif ()
-else()
-    include(external_dependencies/boost.cmake)
 endif()
-
+    
 if(WIN32)
     include(external_dependencies/glfw3.cmake)
 endif()
-
+    
+include(external_dependencies/boost.cmake)
 include(external_dependencies/mujoco.cmake)
 # include(external_dependencies/lcm.cmake) #TODO change from websockets to lcm(udp multicast). Need instalation of lib GLib2 for windows. lcm provides future connection with drake. lcm is MIT+Toyota tested.
